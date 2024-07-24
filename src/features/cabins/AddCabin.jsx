ad4109ff7ea6219f853879e615579e29483d6ea7
Sladjana <csl.bunny.2207@gmail.com>
2024-07-24 22:49:57 +0200

    using modal window for edit cabin

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -26,21 +26,23 @@ import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
 	return (
-		<Modal>
-			<Modal.Open opens="cabin-form">
-				<Button>Add new cabin</Button>
-			</Modal.Open>
-			<Modal.Window name="cabin-form">
-				<CreateCabinForm />
-			</Modal.Window>
+		<div>
+			<Modal>
+				<Modal.Open opens="cabin-form">
+					<Button>Add new cabin</Button>
+				</Modal.Open>
+				<Modal.Window name="cabin-form">
+					<CreateCabinForm />
+				</Modal.Window>
 
-			{/* <Modal.Open opens="table">
+				{/* <Modal.Open opens="table">
 				<Button>Show table</Button>
 			</Modal.Open>
 			<Modal.Window name="table">
 				<CabinTable />
 			</Modal.Window> */}
-		</Modal>
+			</Modal>
+		</div>
 	);
 }
 
