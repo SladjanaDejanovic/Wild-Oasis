871d48372abcdf9b8f7edcd3bc648d7634471edc
Sladjana <csl.bunny.2207@gmail.com>
2024-07-30 23:48:02 +0200

    booking detail

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import {
 } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
 	defaultOptions: {
@@ -45,6 +46,7 @@ function App() {
 						/>
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
+						<Route path="bookings/:bookingId" element={<Booking />} />
 						<Route path="cabins" element={<Cabins />} />
 						<Route path="users" element={<Users />} />
 						<Route path="settings" element={<Settings />} />
---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+	const { bookingId } = useParams();
+
+	const {
+		isLoading,
+		data: booking,
+		error,
+	} = useQuery({
+		queryKey: ["bookking"],
+		queryFn: () => getBooking(bookingId),
+		retry: false, //react query by default tries to fetch data 3 times in case if failed in the beginning, so in some cases that doesn't make much sense, like here, if it failed means there's no data
+	});
+	return { isLoading, error, booking };
+}
