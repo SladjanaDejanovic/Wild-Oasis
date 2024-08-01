37455d1a8e9e9127c274cf6f2e52a4bb1e57ac26
Sladjana <csl.bunny.2207@gmail.com>
2024-08-01 18:55:17 +0200

    delete a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -8,11 +8,14 @@ import ButtonGroup from "../../ui/ButtonGroup";
 import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 import Spinner from "../../ui/Spinner";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
 	display: flex;
@@ -23,6 +26,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
 	const { booking, isLoading } = useBooking();
 	const { checkout, isCheckingOut } = useCheckout();
+	const { deleteBooking, isDeletingBooking } = useDeleteBooking();
 
 	const navigate = useNavigate();
 	const moveBack = useMoveBack();
@@ -66,6 +70,25 @@ function BookingDetail() {
 					</Button>
 				)}
 
+				<Modal>
+					<Modal.Open opens="delete">
+						<Button variation="danger" onClick={moveBack}>
+							Delete booking
+						</Button>
+					</Modal.Open>
+
+					<Modal.Window name="delete">
+						<ConfirmDelete
+							resourceName="booking"
+							disabled={isDeletingBooking}
+							onConfirm={() =>
+								deleteBooking(bookingId, {
+									onSettled: () => navigate(-1),
+								})
+							}
+						/>
+					</Modal.Window>
+				</Modal>
 				<Button variation="secondary" onClick={moveBack}>
 					Back
 				</Button>
