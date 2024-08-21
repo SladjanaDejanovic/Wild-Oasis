1d0a0ea8e42a3b591c8f6f2e7e4ba0c915ec5581
Sladjana <csl.bunny.2207@gmail.com>
2024-08-21 22:52:05 +0200

    pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -39,7 +40,7 @@ function DashboardLayout() {
 			/>
 			<div>Statistics</div>
 			<div>Todays activity</div>
-			<div>Chart stay durations</div>
+			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
 	);
