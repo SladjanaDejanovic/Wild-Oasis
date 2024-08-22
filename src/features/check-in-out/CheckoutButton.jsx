2234d43ef69023ccbbd3cfb7de6af991c2eb110e
Sladjana <csl.bunny.2207@gmail.com>
2024-08-22 20:32:42 +0200

    displaying stays for today

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,11 +1,18 @@
+import { useCheckout } from "./useCheckout";
 import Button from "../../ui/Button";
 
 function CheckoutButton({ bookingId }) {
-  return (
-    <Button variation="primary" size="small">
-      Check out
-    </Button>
-  );
+	const { isCheckingOut, checkout } = useCheckout();
+	return (
+		<Button
+			variation="primary"
+			size="small"
+			onClick={() => checkout(bookingId)}
+			disabled={isCheckingOut}
+		>
+			Check out
+		</Button>
+	);
 }
 
 export default CheckoutButton;
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -17,7 +18,6 @@ const StyledDashboardLayout = styled.div`
 function DashboardLayout() {
 	const { bookings, isLoading: isLoading1 } = useRecentBookings();
 	const {
-		stays,
 		confirmedStays,
 		isLoading: isLoading2,
 		numDays,
@@ -38,8 +38,8 @@ function DashboardLayout() {
 				numDays={numDays}
 				cabinCount={cabins.length}
 			/>
-			<div>Statistics</div>
-			<div>Todays activity</div>
+
+			<TodayActivity />
 			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
