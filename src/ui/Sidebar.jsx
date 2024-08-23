962f0d8c06aca6c3a23fbe027f3998e950fe3db3
Sladjana <csl.bunny.2207@gmail.com>
2024-08-23 22:45:37 +0200

    removing sample data button

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import Logo from "./Logo";
 import MainNav from "./MainNav";
-import Uploader from "../data/Uploader";
+// import Uploader from "../data/Uploader";
 
 const StyleSidebar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -19,7 +19,6 @@ function Sidebar() {
 		<StyleSidebar>
 			<Logo />
 			<MainNav />
-			<Uploader />
 		</StyleSidebar>
 	);
 }
