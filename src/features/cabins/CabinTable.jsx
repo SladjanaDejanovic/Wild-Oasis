b04e32b55e1e43c9a8fdfb4f45583a8344cc72fa
Sladjana Dejanovic <109485412+SladjanaDejanovic@users.noreply.github.com>
2024-09-01 20:41:12 +0200

    Update CabinTable.jsx

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -9,10 +9,7 @@ import Empty from "../../ui/Empty";
 
 // const TableHeader = styled.header`
 // 	display: grid;
-// 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-
-// 	align-items: center;
-
+// 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 
 // 	background-color: var(--color-grey-50);
 // 	border-bottom: 1px solid var(--color-grey-100);
 // 	text-transform: uppercase;
