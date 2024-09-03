80cb1fec65aca5765680fa5c91c8a73d15bb6cf4
Sladjana Dejanovic <109485412+SladjanaDejanovic@users.noreply.github.com>
2024-09-03 22:08:12 +0200

    Update CabinTable.jsx

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -8,7 +8,6 @@ import { useSearchParams } from "react-router-dom";
 import Empty from "../../ui/Empty";
 
 // const TableHeader = styled.header`
-// 	display: grid;
 // 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 
 // 	background-color: var(--color-grey-50);
 // 	border-bottom: 1px solid var(--color-grey-100);
