f70de87b435a778a6c66cf424c36536e6995b8a3
Sladjana Dejanovic <109485412+SladjanaDejanovic@users.noreply.github.com>
2024-09-06 19:19:02 +0200

    Update CabinTable.jsx

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -10,7 +10,6 @@ import Empty from "../../ui/Empty";
 // const TableHeader = styled.header`
 // 	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 
 // 	background-color: var(--color-grey-50);
-// 	border-bottom: 1px solid var(--color-grey-100);
 
 
 function CabinTable() {
