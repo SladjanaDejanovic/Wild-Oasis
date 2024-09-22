51503fc8a0e832eeb3830ba6675114b7d8f97da6
Sladjana <csl.bunny.2207@gmail.com>
2024-09-22 19:21:56 +0200

    comments

--- src/features/check-in-out/useCheckout.js
+++ src/features/check-in-out/useCheckout.js
@@ -11,7 +11,6 @@ export function useCheckout() {
 				status: "checked-out",
 			}),
 
-		// onSuccess recieves data, which is value returned from the function
 		onSuccess: (data) => {
 			toast.success(`Booking #${data.id} successfully checked out`);
 
